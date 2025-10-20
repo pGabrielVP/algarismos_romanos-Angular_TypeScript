c8d182922938feb8ca3b96cffd2d99c26b88f2b8
pGabrielVP <paulogabrielprado@hotmail.com>
2025-10-20 12:15:47 -0300

    Adiciona suporte a caracteres novos
    Adiciona novos algarismos a ROMAN_DECIMAL_MAP.
    Atualiza VALID_ALGARISMS_REGEX para suportar os novos algarismos.
    Atualiza a lógica do conversor para calcular o valor usando os novos algarismos.

--- src/app/constants/roman_decimal.ts
+++ src/app/constants/roman_decimal.ts
@@ -6,5 +6,12 @@ export const ROMAN_DECIMAL_MAP: Map<string, number> = new Map([
     ["C", 100],
     ["D", 500],
     ["M", 1000],
+    ["_I", 1000],
+    ["_V", 5000],
+    ["_X", 10000],
+    ["_L", 50000],
+    ["_C", 100000],
+    ["_D", 500000],
+    ["_M", 1000000],
 ])
-export const VALID_ALGARISMS_REGEX = /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+export const VALID_ALGARISMS_REGEX = /^(_M){0,3}((_C_M)|(_C_D)|(_D)?(_C){0,3})((_X_C)|(_X_L)|(_L)?(_X){0,3})((_I_X)|(_I_V)|(_V)?)M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
--- src/app/services/converter.ts
+++ src/app/services/converter.ts
@@ -11,14 +11,34 @@ export class Converter {
     algarismos = algarismos.toUpperCase();
     let total = 0;
     for (let i = 0; i < algarismos.length; i++) {
-      const currentValue = ROMAN_DECIMAL_MAP.get(algarismos.charAt(i)) ?? 0;
-      const nextValue = (i + 1 < algarismos.length) ? ROMAN_DECIMAL_MAP.get(algarismos.charAt(i + 1)) ?? 0 : 0;
-
-      if (nextValue > currentValue) {
-        total += (nextValue - currentValue);
-        i++;
-      } else {
+      if (algarismos.charAt(i) === "_"
+        && i + 3 < algarismos.length
+        && algarismos.charAt(i + 2) === "_") {
+        const pair = algarismos.charAt(i) + algarismos.charAt(i + 1);
+        const lookaheadPair = algarismos.charAt(i + 2) + algarismos.charAt(i + 3);
+        const currentValue = ROMAN_DECIMAL_MAP.get(pair) ?? 0;
+        const nextValue = ROMAN_DECIMAL_MAP.get(lookaheadPair) ?? 0;
+        if (nextValue > currentValue) {
+          total += (nextValue - currentValue);
+          i = i + 3;
+        } else {
+          total += currentValue;
+          i = i + 1;
+        }
+      } else if (algarismos.charAt(i) === "_") {
+        const pair = algarismos.charAt(i) + algarismos.charAt(i + 1);
+        const currentValue = ROMAN_DECIMAL_MAP.get(pair) ?? 0;
         total += currentValue;
+        i = i + 1;
+      } else {
+        const currentValue = ROMAN_DECIMAL_MAP.get(algarismos.charAt(i)) ?? 0;
+        const nextValue = (i + 1 < algarismos.length) ? ROMAN_DECIMAL_MAP.get(algarismos.charAt(i + 1)) ?? 0 : 0;
+        if (nextValue > currentValue) {
+          total += (nextValue - currentValue);
+          i++;
+        } else {
+          total += currentValue;
+        }
       }
     }
     return total;
